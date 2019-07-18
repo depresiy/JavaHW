963b6938d5b54ce6114616b657334b7b87ae9caa
depresiy <depresiy@mail.ru>
2019-07-18 14:55:13 +0500

    первые правки

--- src/Lesson_1/Marathon/Main.java
+++ src/Lesson_1/Marathon/Main.java
@@ -2,16 +2,9 @@ package Lesson_1.Marathon;
 
 public class Main {
     public static void main(String[] args) {
-        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-        Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
-        for (Competitor c : competitors) {
-            for (Obstacle o : course) {
-                o.doIt(c);
-                if (!c.isOnDistance()) break;
-            }
-        }
-        for (Competitor c : competitors) {
-            c.info();
-        }
+        Course c = new Course(...); // Создаем полосу препятствий
+        Team team = new Team(...); // Создаем команду
+        c.doIt(team); // Просим команду пройти полосу
+        team.showResults(); // Показываем результаты
     }
 }
